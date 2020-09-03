655d644db5e24b6047db0b4cfdb3601ca43ca352
ianrscott2 <ianrscott2@gmail.com>
2020-09-03 16:35:42 -0500

    .

--- NymeBox2/Basic/urls.py
+++ NymeBox2/Basic/urls.py
@@ -5,5 +5,6 @@ from . import views
 urlpatterns = [
     path('', views.nymebox_home, name='nymebox_home'),
     path('app/', views.index, name='index'),
+    path('do_ftp/', views.do_ftp, name='do_ftp'),
     path('ConfigItem/<int:config_id>', views.config_by_id, name='config_by_id')
 ]
--- NymeBox2/Basic/views.py
+++ NymeBox2/Basic/views.py
@@ -4,18 +4,21 @@ from django.http import HttpResponse
 
 from .models import ConfigItem
 
+from .nymebox import NymeBox_Core
+
 def index(request):
         return HttpResponse("Hello World!")
 
-
 def nymebox_home(request):
         config = ConfigItem.objects.get(pk=1)
-        #if config.GIF == "True":
-        #        config.GIF = "Checked"
         return render(request, 'nymebox_home.html', {'config':config})
+
+def do_ftp(request):
+        config = ConfigItem.objects.get(pk=1)
+        #doing_ftp = NymeBox_Core.do_ftp(config.FTP_URL,config.FileType_List)
+        return render(request,'nymebox_output.html', {'output': config})
 
 def config_by_id(request, config_id):
         config = ConfigItem.objects.get(pk=config_id)
         return render(request, 'config_details.html', {'config':config})
-
         #return HttpResponse(f"Config Field: {config.field}, Value: {config.value}")
