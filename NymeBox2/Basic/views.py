9dfb4fbc5d765a3a966f379e799e204435fe8e22
ianrscott2 <ianrscott2@gmail.com>
2020-09-09 16:55:54 -0500

    .

--- NymeBox2/Basic/views.py
+++ NymeBox2/Basic/views.py
@@ -28,16 +28,16 @@ def FTPLog(request):
 @csrf_protect
 def do_ftp(request):
         #return HttpResponse("Trying to do an FTP!")
-        config = ConfigItem.objects.get(pk=1)
-        ftping = NymeBox_Core(config)
+        config = ConfigItem.Manager.raw('SELECT * FROM basic_configitem')
+        ftping = NymeBox_Core(config[0])
         ftping.do_ftp()
         outputfile = open("Basic//FTP_Progress.txt", "r")
         fileContents=outputfile.read()
         return render(request,'nymebox_output.html',{'output':fileContents})
 
 def config_by_id(request, config_id):
-        config = ConfigItem.ProcMode.get(pk=config_id)
-        return render(request, 'config_details.html', {'config':config})
+        config = ConfigItem.Manager.raw('SELECT * FROM basic_configitem')
+        return render(request, 'config_details.html', {'config':config[config_id]})
         #return HttpResponse(f"Config Field: {config.field}, Value: {config.value}")
 
 def last_results(request):
