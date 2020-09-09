f92f53b6a1886fb4b691c662c95d862ce93fba91
ianrscott2 <ianrscott2@gmail.com>
2020-09-09 17:55:42 -0500

    .

--- NymeBox2/Basic/views.py
+++ NymeBox2/Basic/views.py
@@ -14,7 +14,7 @@ def index(request):
 
 def nymebox_home(request):
         config = ConfigItem.Manager.raw('SELECT FtpURL, FileTypeList, FTPUser, FTPPassword, SourceDir, DestDir,ProcMode FROM basic_configitem WHERE ProcMode = %s', [app_mode])
-        #if config != None:
+        if config[0] != None:
         #       print("the config type is: " + str(type(config)) + "\n")
         #       print("the config type is: " + config[0].FtpURL + "\n")
         return render(request, 'nymebox_home.html')#, {'config':config[0]})
